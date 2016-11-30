1b25909d54799e0040e5f34f8275e9ee5ec34b04
Martin Walter <martin.walter@qlik.com>
2016-11-30 09:20:01 +0100

    Rough first implementation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# flow-show
-RSS app for showing off some skills
+# What's this?
+Flow Show is your own personal RSS reader for the latest news on [Dagens Nyheter](https://www.dn.se).
+
+# Running it
+`sudo python -m SimpleHTTPServer 80`
+
+# Made with inspiration and love from
+https://material.angularjs.org/
+https://materialdesignicons.com
---
+++ js/app.js
@@ -0,0 +1,54 @@
+var flowshow = angular.module("flowshow", ['ui.router', 'ngMaterial']);
+
+flowshow.config(function($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+    $stateProvider
+        .state('news', {
+            url: '/',
+            templateUrl: 'templates/news.html',
+            controller: 'NewsController'
+        });
+    $urlRouterProvider.otherwise('/');
+
+    $mdThemingProvider.theme('default')
+        .primaryPalette('red');
+});
+
+flowshow.controller("NewsController", function($scope, FeedService) {
+
+
+    FeedService.getFeed("http://www.dn.se/nyheter/m/rss/")
+        .then(function(response) {
+            $scope.news = response.data.responseData.feed;
+            console.log(response);
+        });
+});
+
+flowshow.controller("FooterController", function($scope) {
+    $scope.links = [
+        {
+            name: "E-mail",
+            url: "mailto:martin.walter.swe@gmail.com",
+            icon: "mdi-email-outline"
+        },
+        {
+            name: "GitHub",
+            url: "https://github.com/marols",
+            icon: "mdi-github-circle"
+        },
+        {
+            name: "LinkedIn",
+            url: "https://se.linkedin.com/in/mwalter84",
+            icon: "mdi-linkedin-box"
+        }
+    ]
+    ;
+});
+
+
+flowshow.factory('FeedService',['$http',function($http){
+    return {
+        getFeed : function(url){
+            return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+        }
+    }
+}]);
